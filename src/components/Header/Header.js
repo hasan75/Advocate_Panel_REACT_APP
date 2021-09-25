14fcc2e8aaf3408d3204a93ee47765b1f6ec93ce
Hasan75 <hasanjab14@gmail.com>
2021-09-26 05:03:19 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>Recruiting The Advocacy Team for Ekhon-Organization</h2>
+            <h3> Our Organization needs your help to select the best advocates from the list whome will be the consultants for our organization</h3>
+            <h2>Total Budget of the Recruiting: $10000</h2>
+        </div>
+    );
+};
+
+export default Header;
